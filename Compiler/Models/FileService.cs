813060fad6b9696403384d24812864b071f8d7cf
Dmewtriy <dankin.dm@mail.ru>
2026-02-26 17:17:37 +0700

    Добавлен класс для работы с путями файлов

---
+++ Compiler/Models/FileService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Models
+{
+    public class FileService
+    {
+        public string CurrentFilePath { get; private set; }
+
+        public string OpenFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("Файл не найден.");
+
+            CurrentFilePath = filePath;
+            return File.ReadAllText(filePath);
+        }
+
+        public void SaveFile(string filePath, string content)
+        {
+            File.WriteAllText(filePath, content);
+            CurrentFilePath = filePath;
+        }
+
+        public void ClearCurrentFile()
+        {
+            CurrentFilePath = null;
+        }
+    }
+}
