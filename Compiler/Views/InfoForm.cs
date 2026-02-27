19b19b4c73fc1ce88eed3db0595a546001faf900
Dmewtriy <dankin.dm@mail.ru>
2026-02-27 16:20:43 +0700

    Создана форма для отображения меню "Справка"
    Создана одна шаблонная форма InfoForm, в которой находится WebBrowser
    контрол для отображения текста в разметке html
    Добавлена модель InfoService, в которой хранится текст в разметке Markdown
    В MainController передается InfoService и добавлен метод для конвертирования
    Markdown в html, создания формы и отрисовки текста

---
+++ Compiler/Views/InfoForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compiler.Views
+{
+    public partial class InfoForm : Form
+    {
+        private WebBrowser webBrowser; // Создаем переменную в коде
+
+        public InfoForm(string title, string htmlContent)
+        {
+            InitializeComponent();
+
+            this.Text = title;
+            this.Size = new Size(800, 600);
+            this.StartPosition = FormStartPosition.CenterParent;
+
+            // Инициализируем браузер программно
+            webBrowser = new WebBrowser();
+            webBrowser.Dock = DockStyle.Fill; // Растягиваем на все окно
+
+            // Добавляем его на форму
+            this.Controls.Add(webBrowser);
+
+            // Загружаем контент
+            webBrowser.DocumentText = htmlContent;
+        }
+    }
+}
