c74bdf6e220433c82c8c3a6578e60472607cf024
Dmewtriy <dankin.dm@mail.ru>
2026-03-22 23:30:27 +0700

    refactor: изменить движок для формы браузера

--- Compiler/Views/InfoForm.cs
+++ Compiler/Views/InfoForm.cs
@@ -1,26 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
 
 namespace Compiler.Views
 {
     public partial class InfoForm : Form
     {
-        private WebBrowser webBrowser;
+        private WebView2 webBrowser;
+        private string _htmlToLoad;
 
         public InfoForm(string title, string htmlBody)
         {
             InitializeComponent();
-            string htmlContent = $@"
+
+            this.Text = title;
+            this.Size = new Size(800, 600);
+            this.StartPosition = FormStartPosition.CenterParent;
+
+            webBrowser = new WebView2 { Dock = DockStyle.Fill };
+            this.Controls.Add(webBrowser);
+
+            _htmlToLoad = $@"
             <html>
             <head>
-                <meta http-equiv='X-UA-Compatible' content='IE=edge' />
                 <style>
                     body {{ font-family: 'Segoe UI', sans-serif; padding: 20px; line-height: 1.6; }}
                     code {{ background-color: #f4f4f4; padding: 2px 4px; border-radius: 4px; }}
@@ -32,16 +35,25 @@ namespace Compiler.Views
                 {htmlBody}
             </body>
             </html>";
-            this.Text = title;
-            this.Size = new Size(800, 600);
-            this.StartPosition = FormStartPosition.CenterParent;
 
-            webBrowser = new WebBrowser();
-            webBrowser.Dock = DockStyle.Fill;
+            InitializeAsync();
+        }
 
-            this.Controls.Add(webBrowser);
+        async void InitializeAsync()
+        {
+            try
+            {
+                await webBrowser.EnsureCoreWebView2Async(null);
 
-            webBrowser.DocumentText = htmlContent;
+                webBrowser.CoreWebView2.Profile.PreferredColorScheme =
+                    Microsoft.Web.WebView2.Core.CoreWebView2PreferredColorScheme.Light;
+
+                webBrowser.CoreWebView2.NavigateToString(_htmlToLoad);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка инициализации браузера: {ex.Message}");
+            }
         }
     }
 }
